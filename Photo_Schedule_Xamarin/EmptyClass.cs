1cc05a8b4ad14ada44fb0e3e9ea8ac1d22ff90e4
none <shige053@me.com>
2019-04-26 17:58:57 +0900

    Xamarin開発開始

---
+++ Photo_Schedule_Xamarin/EmptyClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Photo_Schedule_Xamarin
+{
+    public class EmptyClass
+    {
+        public int test1 = 10;
+        public int test2 = 20;
+
+        public void test3()
+        {
+            System.Diagnostics.Debug.WriteLine("デバッグ初テスト" + test1);
+            System.Diagnostics.Trace.WriteLine("Xamarinもっと簡単にして！" + test2);
+            System.Console.WriteLine("Console.WriteLine による出力です");
+        }
+    }
+}
+デバッグ
